2795633f7901e18d9f5c2b2cce4f40d8dadfb010
delta-kor <deltakbot1@gmail.com>
2020-09-22 22:31:48 +0900

    Fix event listener

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import http, { Server } from 'http';
+import { EventEmitter } from 'events';
 import express, { Application } from 'express';
-import { AppEvent } from './types/event';
 import Kernel from './middlewares/kernel';
 import ApiRouter from './routes/api';
 import WebRouter from './routes/web';
+import { AppEvent } from './types/event';
 
-export default class App extends AppEvent {
+export declare interface App {
+  emit<E extends keyof AppEvent>(event: E, ...args: Parameters<AppEvent[E]>): boolean;
+  on<E extends keyof AppEvent>(event: E, listener: AppEvent[E]): this;
+  once<E extends keyof AppEvent>(event: E, listener: AppEvent[E]): this;
+}
+
+export class App extends EventEmitter {
   public port: number;
   private readonly application: Application;
   private readonly server: Server;
--- src/types/event.d.ts
+++ src/types/event.d.ts
@@ -1,21 +1,7 @@
-import { EventEmitter } from 'events';
-
-declare class AppEvent extends EventEmitter {
-  public emit(event: 'init'): boolean;
-  public emit(event: 'ready'): boolean;
-  public emit(event: 'deploy', port: number): boolean;
-  public emit(event: 'mount_middleware', middlewares: number): boolean;
-  public emit(event: 'mount_route'): boolean;
-
-  public on(event: 'init', listener: () => void): this;
-  public on(event: 'ready', listener: () => void): this;
-  public on(event: 'deploy', listener: (port: number) => void): this;
-  public on(event: 'mount_middleware', listener: (middlewares: number) => void): this;
-  public on(event: 'mount_route', listener: () => void): this;
-
-  public once(event: 'init', listener: () => void): this;
-  public once(event: 'ready', listener: () => void): this;
-  public once(event: 'deploy', listener: (port: number) => void): this;
-  public once(event: 'mount_middleware', listener: (middlewares: number) => void): this;
-  public once(event: 'mount_route', listener: () => void): this;
+export interface AppEvent {
+  init: () => void;
+  ready: () => void;
+  deploy: (port: number) => void;
+  mount_middleware: (middlewares: number) => void;
+  mount_route: () => void;
 }
