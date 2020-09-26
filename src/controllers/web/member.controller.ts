7954c5d30ba37711c413732ece03e6d704aa80a6
delta-kor <deltakbot1@gmail.com>
2020-09-26 19:39:09 +0900

    Add title, description

--- src/controllers/web/member.controller.ts
+++ src/controllers/web/member.controller.ts
@@ -3,11 +3,19 @@ import Transform from '../../utils/transform';
 
 export default class MemberController {
   static signup(req: Request, res: Response): any {
-    res.render('member/signup', { key: Transform.encode(req.ticket.public) });
+    res.render('member/signup', {
+      title: 'Signup',
+      description: 'Signup to simple-sns',
+      key: Transform.encode(req.ticket.public),
+    });
     return true;
   }
   static login(req: Request, res: Response): any {
-    res.render('member/login');
+    res.render('member/login', {
+      title: 'Login',
+      description: 'Login to simple-sns',
+      key: Transform.encode(req.ticket.public),
+    });
     return true;
   }
 }
