f9d31bace59f08a902b442b42e82421c8c131309
delta-kor <deltakbot1@gmail.com>
2020-09-27 09:29:18 +0900

    Add request validating

--- src/controllers/api/member.controller.ts
+++ src/controllers/api/member.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import validator from 'validator';
+import Output, { Status } from '../../providers/output';
 
 export interface SignupPayload {
   email: string;
@@ -9,6 +11,22 @@ export interface SignupPayload {
 export default class MemberController {
   static signup(req: Request, res: Response): any {
     const body: SignupPayload = req.body;
-    console.log(body);
+
+    if (!validator.isEmail(body.email)) {
+      Output.reject(res, Status.SIGNUP_INVALID_EMAIL);
+      return false;
+    }
+
+    if (body.password.length < 8) {
+      Output.reject(res, Status.SIGNUP_SHORT_PASSWORD);
+      return false;
+    }
+
+    if (!validator.equals(body.password, body.confirm)) {
+      Output.reject(res, Status.SIGNUP_PASSWORD_UNMATCH);
+      return false;
+    }
+
+    Output.resolve(res);
   }
 }
