d0fcd64a45654c820b79b2274535bb570189629b
delta-kor <deltakbot1@gmail.com>
2020-11-02 23:10:26 +0900

    Fix view

--- src/middlewares/view.ts
+++ src/middlewares/view.ts
@@ -2,7 +2,7 @@ import { Application } from 'express';
 
 export default class View implements Middleware {
   public static mount(application: Application): void {
-    application.set('views', 'views/pages');
     application.set('view engine', 'pug');
+    application.set('views', 'views/pages');
   }
 }
