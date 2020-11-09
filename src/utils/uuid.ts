d2563f35b327534359af2ded71ac31cd81b22944
delta-kor <deltakbot1@gmail.com>
2020-11-09 14:41:29 +0900

    Fix uuid

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 export default class UUID {
-  public static generate(length: number = 16): string {
+  public static generate(length: number = 8): string {
     return crypto.randomBytes(length / 2).toString('hex');
   }
 }
